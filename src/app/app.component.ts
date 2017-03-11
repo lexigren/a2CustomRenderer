4a5cbec118932074479636b51b6cd34e72965f20
lexigren <lexigren@gmail.com>
2017-03-11 16:54:10 +0200

    test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //test comment
   title = 'app works!';
   arr = ['one', 'two', 'three'];
 }
